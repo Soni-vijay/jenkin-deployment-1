18c99e772c43ed18cc115e947b376285ecd2f478
vijay <vijayrs.soni@gmail.com>
2024-04-13 18:00:40 +0530

    push jenkin file

---
+++ jenkin-setup.sh
@@ -0,0 +1,36 @@
+AWS root user:
+vijaysoni-aws-v1/vijayrkumar.soni@gmail.com/Vj@Ra3363
+dockerhub login:
+user name: vijaysoni1
+password: vijay@3363 
+
+Root Login url:
+https://vijaysoni-aws-v1.signin.aws.amazon.com/console
+
+AWS user: 
+Vijay/Sam@3363
+AWS Cli setup:
+key
+AKIARU5UGCD4Y22UWZVS
+values:
+XE0sLqlPIoVx82zqsJ4wvwyzxgqMyqTCsIKMaDho
+
+
+https://drive.google.com/drive/folders/1R_jZ964XaphdgbaRap4QPsXOhV7OHJZB?usp=share_link
+
+Hadoop password:
+maria_dev/maria_dev
+
+
+new aws account: vijay soni
+vs9701@gmail.com/Vj@Sa9970 
+https://654654332046.signin.aws.amazon.com/console
+
+https://vijay-soni-root.signin.aws.amazon.com/console
+Vijay_master/Sam@9970
+
+ssh -i /e/Projects/AWS/Aws-Key/CICD-pipeline.pem ubuntu@ec2-100-26-233-18.compute-1.amazonaws.com
+
+
+s3://guruskrupa-vpro-arts/vprofile-v2.war
+
